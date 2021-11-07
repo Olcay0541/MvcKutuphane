e81b7ee97c6c1d61051208710427a0b81544f4b1
Olcay0541 <81268022+Olcay0541@users.noreply.github.com>
2021-11-07 21:23:44 +0300

    Hafta1

---
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+namespace MvcKutuphane.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        DbKutuphaneEntities db = new DbKutuphaneEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblKategori.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KategoriEkle(TblKategori p)
+        {
+            db.TblKategori.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori = db.TblKategori.Find(id);
+            db.TblKategori.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktg = db.TblKategori.Find(id);
+            return View("KategoriGetir", ktg);
+        }
+        public ActionResult KategoriGuncelle(TblKategori p)
+        {
+            var ktg = db.TblKategori.Find(p.KategoriID);
+            ktg.KategoriAD = p.KategoriAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcKutuphane/Controllers/YazarController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+namespace MvcKutuphane.Controllers
+{
+    public class YazarController : Controller
+    {
+        // GET: Yazar
+        DbKutuphaneEntities db = new DbKutuphaneEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblYazar.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YazarEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YazarEkle(TblYazar p)
+        {
+            db.TblYazar.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult YazarSil(int id)
+        {
+            var yazar = db.TblYazar.Find(id);
+            db.TblYazar.Remove(yazar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult YazarGetir(int id)
+        {
+            var yazar = db.TblYazar.Find(id);
+            return View("YazarGetir", yazar);
+        }
+    }
+}
